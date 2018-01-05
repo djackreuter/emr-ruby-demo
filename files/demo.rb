c0f490fabc736bd9ec4cf80808bead296db8cb01
Jack Reuter <djreuter45@gmail.com>
2018-01-05 10:45:52 -0700

    added methods for using blocks and params to array

--- files/demo.rb
+++ files/demo.rb
@@ -4,5 +4,23 @@ def foo(num1, num2)
 end
 
 bar = [100, 24]
-
+# pass array using splat
 puts foo(*bar)
+
+# params to array
+def bizz(x, y, z)
+  [x * x, y - y, z + x]
+end
+
+puts bizz(4, 3, 1)
+
+# block as param
+def my_method
+  puts 'method called'
+  yield(1)
+  puts 'back to method'
+end
+
+my_method do |e|
+  puts "block called #{e} time"
+end
