b7fdb2e381428309b344e4d1752bc1eafa3afe2e
Jack Reuter <djreuter45@gmail.com>
2018-01-05 11:46:52 -0700

    added demo of proc and lambda

--- files/demo.rb
+++ files/demo.rb
@@ -24,3 +24,21 @@ end
 my_method do |e|
   puts "block called #{e} time"
 end
+
+# proc
+multiply_by10 = proc { |n| n * 10 }
+
+puts multiply_by10.call(4)
+
+# single line lambda
+greeting = ->(name) { "Hello, #{name}" }
+
+puts greeting.call('Jack')
+
+# multiline lambda
+some_number = lambda do |a, b|
+  num1 = a * 5
+  num1 - b / 2
+end
+
+puts some_number.call(4, 5)
