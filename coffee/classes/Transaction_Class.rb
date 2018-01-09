6a76650d8f9973e0653ec45b2b2fc82616774f16
Jack Reuter <djreuter45@gmail.com>
2018-01-09 11:57:28 -0700

    added method to transaction class

--- coffee/classes/Transaction_Class.rb
+++ coffee/classes/Transaction_Class.rb
@@ -22,8 +22,23 @@ class Transaction
     end
   end
 
+  def prompt
+    if coffee.incomplete?
+      puts coffee.next_question
+      gets.chop
+    else
+      puts coffee.description
+    end
+  end
+
   def order_total
     puts "Your #{@size} coffee comes to #{@price}"
   end
 
 end
+
+total = Transaction.new('large', nil)
+
+total.coffee_price
+
+puts total.order_total
