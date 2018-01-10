0ba6afb40fdd1045f21fe85b5d76818833316710
Jack Reuter <djreuter45@gmail.com>
2018-01-10 12:53:49 -0700

    added some test methods to coffee_test

--- coffee/classes/test/coffee_test.rb
+++ coffee/classes/test/coffee_test.rb
@@ -1,6 +1,41 @@
-# unit test for coffee application
 require 'minitest/autorun'
+require '../Coffee.rb'
 
-class Test < Minitest::Test
- 
+# unit test for coffee application
+class CoffeeTest < Minitest::Test
+
+  def setup
+    coffee = Coffee.new
+    @valid_roast = 'dark'
+    @valid_sugar = '0'
+    @valid_cream = 'yes'
+    @valid_size = 'small'
+  end
+
+  def test_incomplete?
+    coffee = Coffee.new
+    assert_equal true, coffee.incomplete?
+  end
+
+  def test_next_question
+    coffee = Coffee.new
+    coffee.roast = @valid_roast
+    coffee.sugar = @valid_sugar
+    coffee.cream = @valid_cream
+    coffee.size = @valid_size
+    assert_equal "Sorry, I didn't quite get that", coffee.next_question
+  end
+
+  def test_process_input
+    coffee = Coffee.new
+    coffee.roast = @valid_roast
+    coffee.sugar = @valid_sugar
+    coffee.cream = @valid_cream
+    coffee.size = @valid_size
+    assert_equal @valid_roast, coffee.roast
+    assert_equal @valid_sugar, coffee.sugar
+    assert_equal @valid_cream, coffee.cream
+    assert_equal @valid_size, coffee.size
+  end
+
 end
