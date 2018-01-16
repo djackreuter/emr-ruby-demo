ca8d89d331d66e14d39ffeca0bfe393108b388fa
Jack Reuter <djreuter45@gmail.com>
2018-01-16 13:16:10 -0700

    added user input for calender and created a class method to simplify calling everything

--- erb/calender.rb
+++ erb/calender.rb
@@ -5,14 +5,27 @@ require 'date'
 class Calender
   attr_accessor :beg_of_month, :end_of_month
 
+  def self.run
+    instance = Calender.new
+    instance.cal_dates
+    instance.build
+  end
+
   def read
     file = File.open('./templates/calender.erb')
     contents = file.read
   end
 
+  def cal_dates
+    print 'Enter year: '
+    @year = gets.to_i
+    print 'Enter month: '
+    @month = gets.to_i
+  end
+
   def build
-    @beg_of_month = Date.civil(gets.to_i, gets.to_i, 1)
-    @end_of_month = Date.civil(gets.to_i, gets.to_i, -1)
+    @beg_of_month = Date.civil(@year, @month, 1)
+    @end_of_month = Date.civil(@year, @month, -1)
     puts ERB.new("#{read}", nil, '>').result(binding)
   end
 end
