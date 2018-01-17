c913dc0c2ee43ad3b3fa0999b7f5a626eb0a8681
Jack Reuter <djreuter45@gmail.com>
2018-01-17 09:10:18 -0700

    added starter app.rb and config file for Sinatra

--- app.rb
+++ app.rb
@@ -1,2 +1,14 @@
 require 'sinatra'
+require_relative 'lib/calender.rb'
 
+# Multiply = Multi::Multiply
+# Main class for App
+# class Main < Sinatra::Base
+  # extend Multiply
+
+  get '/multiply' do
+    @x = (1..12)
+    @y = (1..12)
+    erb :multiplication
+  end
+# end
--- config.ru
+++ config.ru
@@ -1,2 +1,2 @@
 require './app'
-run Multiply
+# run Main
