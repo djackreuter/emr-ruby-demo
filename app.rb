9ff39624f106d0210442278254c0c9c400c52f67
Jack Reuter <djreuter45@gmail.com>
2018-01-17 09:58:15 -0700

    cleaned up app.rb

--- app.rb
+++ app.rb
@@ -1,14 +1,9 @@
 require 'sinatra'
 require_relative 'lib/calender.rb'
 
-# Multiply = Multi::Multiply
-# Main class for App
-# class Main < Sinatra::Base
-  # extend Multiply
+get '/multiply' do
+  @x = (1..12)
+  @y = (1..12)
+  erb :multiplication
+end
 
-  get '/multiply' do
-    @x = (1..12)
-    @y = (1..12)
-    erb :multiplication
-  end
-# end
